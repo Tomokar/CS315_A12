3db2b8b13fe5da2bbd276f0b4b54312f4f8cef0e
tomokar <ncm17b@acu.edu>
2022-11-15 09:23:38 -0600

    Commit 1: Images

--- CS315_A12/ViewController.swift
+++ CS315_A12/ViewController.swift
@@ -5,15 +5,32 @@
 //  Created by Noah Massie on 11/15/22.
 //
 
-import UIKit
+import UIKit;
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class ViewController: UIViewController
+{
+    @IBOutlet var diceImageView: UIImageView!;
+    @IBOutlet var criticalLabel: UILabel!;
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad();
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buttonGO()
+    {
+        rollDice();
+    }
+    
+    func rollDice()
+    {
+        print("Rolled");
+        
+        let rolledNum = Int.random(in: 1...20);
+        let imageName = "d\(rolledNum)";
+        
+        diceImageView.image = UIImage(named: imageName);
+        
+    }
 }
-
