de1be6937b4caac07d1d6b8d951ee6ccf57f9708
tomokar <93736469+Tomokar@users.noreply.github.com>
2022-11-16 12:33:54 -0600

    Commit 3: Sounds and functions complete.

--- CS315_A12/ViewController.swift
+++ CS315_A12/ViewController.swift
@@ -37,19 +37,30 @@ class ViewController: UIViewController
     
     func rollDice(i: Int)
     {
+        var pathToSound = String("");
+//        var url = URL(fileURLWithPath: pathToSound);
+        var fileName = String("rolldice");
+        var fileType = String("mp3");
+        
         switch(i)
         {
         case 0:
             print("Rolled");
+            fileName = "rolldice";
 //            let pathToSound = Bundle.main.path(forResource: "rolldice", ofType: "mp3")!;
             break;
         case 1:
             print("Shaken");
+            fileName = "torpedo"
 //            let pathToSound = Bundle.main.path(forResource: "torpedo", ofType: "mp3")!;
             break;
         default:
             break;
         }
+        
+//        pathToSound = makePath(name: fileName, type: fileType);
+        
+//        playSound(/*url: url, */path: pathToSound);
         
         let rolledNum = Int.random(in: 1...20);
         let imageName = "d\(rolledNum)";
@@ -58,11 +69,13 @@ class ViewController: UIViewController
         {
         case 1:
 //            let pathToSound = Bundle.main.path(forResource: "failwah", ofType: "mp3")!;
+            fileName = "failwah";
             criticalLabel.text = "Oof";
             criticalLabel.isHidden = false;
             break;
         case 20:
 //            let pathToSound = Bundle.main.path(forResource: "zfanfare", ofType: "mp3")!;
+            fileName = "zfanfare";
             criticalLabel.text = "Nice";
             criticalLabel.isHidden = false;
             break;
@@ -71,18 +84,32 @@ class ViewController: UIViewController
             break;
         }
         
-//        let url = URL(fileURLWithPath: pathToSound);
-//
-//        do
-//        {
-//            audioPlayer = try AVAudioPlayer(contentsOf: url);
-//            audioPlayer?.play();
-//        }
-//        catch
-//        {
-//            print("error babe");
-//        }
+        pathToSound = makePath(name: fileName, type: fileType);
+        
+        playSound(/*url: url, */path: pathToSound);
         
         diceImageView.image = UIImage(named: imageName);
+    }
+    
+    func makePath(name: String, type: String) -> String
+    {
+        var returnPath = Bundle.main.path(forResource: name, ofType: type)!;
+        
+        return returnPath;
+    }
+    
+    func playSound(/*url: URL, */path: String)
+    {
+        let url = URL(fileURLWithPath: path);
+        
+        do
+        {
+            audioPlayer = try AVAudioPlayer(contentsOf: url);
+            audioPlayer?.play();
+        }
+        catch
+        {
+            print("error babe");
+        }
     }
 }
